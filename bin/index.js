1437c7e28472255897f4476337f38cb91dd435f0
Martin Egan <martingegan@gmail.com>
2018-09-05 14:55:50 +0100

    Updates

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,15 @@
-'use strict';
+#!/usr/bin/env node
+'use strict'
 
-var cli = require('commander');
-var pkg = require('../package.json');
-var bumpAction = require('./actions/bump');
-cli.version(pkg.version, '-v, --version');
+const cli = require('commander')
+const pkg = require('../package.json')
+const bumpAction = require('../lib/actions/bump')
+cli.version(pkg.version, '-v, --version')
 
 cli
 // Accepted Increments: major, premajor, minor, preminor, patch, prepatch, or prerelease
-.command('bump [increment]').option('-i, --identifier <value>', 'Release identifier').action(bumpAction);
+  .command('bump [increment]')
+  .option('-i, --identifier <value>', 'Release identifier')
+  .action(bumpAction)
 
-cli.parse(process.argv);
+cli.parse(process.argv)
